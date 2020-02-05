f7de89500cb59e901ce8175e2585cdfc19fdcd73
Philani Mkhize <31360347+Jamakasilwane@users.noreply.github.com>
2020-02-05 10:27:02 +0200

    initial commit

---
+++ function_two.py
@@ -0,0 +1,44 @@
+gauteng = [39660.0,
+            36024.0,
+            32127.0,
+            39488.0,
+            18422.0,
+            23532.0,
+            8842.0,
+            37416.0,
+            16156.0,
+            18730.0,
+            19261.0,
+            25275.0]
+
+numbers = []
+def five_num_summ(items):
+    for i in items:
+        numbers.append(i)
+    sort_numbers = sorted(numbers)
+    minimum = sort_numbers[0]
+    maximum = sort_numbers[-1]
+#    print(sort_numbers)
+#    print(minimum)
+#    print(maximum)
+    len_numbers = len(sort_numbers)
+    index = (len_numbers - 1)//2
+    if (len_numbers % 2 == 0):
+        med = (sort_numbers[index] + sort_numbers[index+1])/2.0
+#        print(med)
+    else:
+        med = sort_numbers[index]
+#        print(med)
+    q1_len = len(range(0,index))
+    q1_range = (q1_len-1)//2
+    if q1_len % 2 == 0:
+        q1 = (sort_numbers[q1_range] + sort_numbers[(q1_range+1)])/20
+#       print(q1)
+    else:
+        q1 = sort_numbers[q1_range]
+#        print(q1)
+    return {"Minimum" : minimum, "median" : med, "maximum" : maximum, " 1st quartile" : q1}
+#q2_len = len(range(int(index),-1))
+#print(q2_len)
+
+print(five_num_summ(gauteng))
