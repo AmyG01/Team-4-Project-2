fcfdf306bbd6f25770adcf51b95365d40651b23a
Sibonelo.Malakiya <sbonelojunior@gmail.com>
2020-02-08 23:15:36 +0200

    improvements based on autograde tests

--- function_one_ft.py
+++ function_one_ft.py
@@ -1,21 +1,7 @@
-# Some information explaining what does the function calculate
-# 
-# 
+"""A function that calculates the mean, median, variance, standard deviation, 
+minimum and maximum of list of items"""
 
-# DATASET
-gauteng = [39660.0,
-            36024.0,
-            32127.0,
-            39488.0,
-            18422.0,
-            23532.0,
-            8842.0,
-            37416.0,
-            16156.0,
-            18730.0,
-            19261.0,
-            25275.0]
-            
+          
 ###FUNCTION START
 def dictionary_of_metrics(items) :
 
@@ -25,36 +11,36 @@ def dictionary_of_metrics(items) :
         s = s + value
         count = count + 1
 
-        the_mean = round ( s / count , 2) #mean_calculation
+        the_mean = round ( s / count , 2) 
 
 
     sorted_items = sorted(items)
 
     if count % 2 == 1:
-        return sorted_items[round(count+1)/2-1]
+        return sorted_items[int(round(count+1)/2-1)]
     else:
-            lower = sorted_items[round(count/2-1)  ]
-            upper = sorted_items[round(count/2)]
+            lower = sorted_items[int(round(count/2-1))]
+            upper = sorted_items[int(round(count/2))]
 
-    the_median = (lower + upper) / 2  #median_calculation
-            
+    the_median = (lower + upper) / 2
 
     sum_of_sqz = 0
     for j in items :
         sqrz_calc = (j - the_mean)**2
         sum_of_sqz = sum_of_sqz + sqrz_calc
             
-    the_varience = round(sum_of_sqz / (count - 1), 2)   #varience_calculation
+    the_varience = round(sum_of_sqz / (count - 1), 2)   
 
 
-    the_standard_dev = round((the_varience)**(1/2), 2)  #the_standard_deviation_calculation
+    the_standard_dev = round((the_varience)**(1/2), 2)  
 
     the_min = sorted_items[0] 
 
     the_max = sorted_items[count - 1]
 
-    matrics_dict = {'MEAN':the_mean,'MEDIAN': the_median,'VARIENCE':the_varience,
-    'STARNDARD DEVIATION':the_standard_dev,'MIN':the_min,'MAX':the_max}
+    dict = {'mean':the_mean,'median': the_median,'varience':the_varience,
+    'standard deviation':the_standard_dev,'min':the_min,'max':the_max}
 
-    return(matrics_dict)
+    return dict
+
 ### FUNTION END
