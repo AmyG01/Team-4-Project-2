d58903b5e40d127ca612debc9959a094b340ccc3
SiboneloJunior <60383036+SiboneloJunior@users.noreply.github.com>
2020-02-11 12:21:54 +0200

    assigned the median variable on line 20 so it can be returned

--- function_one_ft.py
+++ function_one_ft.py
@@ -12,25 +12,24 @@ def dictionary_of_metrics(items) :
         count = count + 1
 
         the_mean = round ( s / count , 2) 
-
+    
 
     sorted_items = sorted(items)
 
     if count % 2 == 1:
-        return sorted_items[int(round(count+1)/2-1)]
+            the_median = sorted_items[int(round(count+1)/2-1)]
     else:
             lower = sorted_items[int(round(count/2-1))]
             upper = sorted_items[int(round(count/2))]
 
-    the_median = (lower + upper) / 2
-
+            the_median = (lower + upper) / 2
+    
     sum_of_sqz = 0
     for j in items :
         sqrz_calc = (j - the_mean)**2
         sum_of_sqz = sum_of_sqz + sqrz_calc
-            
-    the_varience = round(sum_of_sqz / (count - 1), 2)   
 
+    the_varience = round(sum_of_sqz / (count - 1), 2)  
 
     the_standard_dev = round((the_varience)**(1/2), 2)  
 
@@ -38,9 +37,9 @@ def dictionary_of_metrics(items) :
 
     the_max = sorted_items[count - 1]
 
-    dict = {'mean':the_mean,'median': the_median,'varience':the_varience,
-    'standard deviation':the_standard_dev,'min':the_min,'max':the_max}
+    dict = {'mean':the_mean,'median': the_median,'var':the_varience,
+    'std':the_standard_dev,'min':the_min,'max':the_max}
 
-    return dict
+    return dict 
 
 ### FUNTION END
