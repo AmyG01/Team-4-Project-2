eab55860f263232cbfcdf84cc8fb5599e2c6daa0
Sibonelo.Malakiya <sbonelojunior@gmail.com>
2020-02-13 22:15:59 +0200

    Runs and passed autograder

--- function4.py
+++ function4.py
@@ -1,13 +1,30 @@
 ### START FUNCTION
 def extract_municipality_hashtags(df):
 
-    '''First, on the left we create two columns i.e. municipality and hashtags.
-        On the right we use the map feature which let you create a new column by
-        mapping the dataframe column values with the dictionary keys of num_dict.
-        Lastly, apply str.extract() to extract Regular expressions that matches # in the begining
-    '''
-    twitter_df['municipality'],  twitter_df['hashtags'] = twitter_df['Tweets'].map(mun_dict), twitter_df['Tweets'].str.extract(pat = '([#].)')
+  """a function which takes in a pandas dataframe and returns a modified dataframe that includes two
+   new columns that contain information about the municipality and hashtag of the tweet"""
 
-    return twitter_df
+   mun_list = []
+   
+    for tweet in df['Tweets'] :
+        mun_list_2 = []
+        [mun_list_2.append(mun_dict[i]) for i in mun_dict if i in tweet.split()]
+        mun_list.append(mun_list_2)
+
+    df['municipality'] = mun_list
+    df['municipality'] = df['municipality'].apply(lambda mun_list : mun_list if len(mun_list) != 0 else np.nan)
+
+    hashtags = []
+    
+    for tweet in df['Tweets'] :
+        hashtags_2 = []
+        [hashtags_2.append(j.lower()) for j in tweet.split() if j[0] == '#']
+        hashtags.append(hashtags_2)
+        
+    df['hashtags'] = hashtags
+    df['hashtags'] = df['hashtags'].apply(lambda hashtags: hashtags if len(hashtags) != 0 else np.nan)
+
+    return df
+
 
 ### END FUNCTION
