75c1a88302c9919acc6ab1f95fb9f3281bad09c4
Lizwi Khanyile <lizwie.khanyile@gmail.com>
2020-02-14 16:15:16 +0200

    function6 complete and runs as expected, need to be checked on auto grade

--- function-six.py
+++ function-six.py
@@ -1,7 +1,28 @@
-def word_spliter(df):
-  result = []
-  l1 = df['Tweets']
-  for tweet in l1:
-    result.append(tweet.lower().split(' '))
-  df['Split words'] = result
-  return df
+### START FUNCTION
+def word_splitter(df):
+    
+    #spliting tweets sentences in Tweets column
+    for tweet in df['Tweets']:
+        tweet = df['Tweets'].str.split()
+
+    #forming a Series of splited tweets as a list of lists   
+    splited_tweets = pd.Series(tweet)
+
+    #adding a new column "Split Tweets" and asigning it to a series "splited_tweets"
+    df['Split Tweets'] = splited_tweets
+
+    #converting rows of "Split Tweets" column to lowercase 
+    df['Split Tweets']= df['Split Tweets'].astype(str).str.lower()
+ 
+    return df
+
+### END FUNCTION
+
+###### Privious Solution################
+# def word_spliter(df):
+#   result = []
+#   l1 = df['Tweets']
+#   for tweet in l1:
+#     result.append(tweet.lower().split(' '))
+#   df['Split words'] = result
+#   return df
