b3f9b262a9dd5accbf58d124413299877f44b6d4
REFENTSE-MOTLGELWA <60340826+REFENTSE-MOTLGELWA@users.noreply.github.com>
2020-02-18 16:11:04 +0200

    Cleaned Funtion 6
    Cleaned PEP8

--- function-six.py
+++ function-six.py
@@ -1,11 +1,14 @@
 ### START FUNCTION
 def word_splitter(df):
-    
-    """A function which splits the sentences in a dataframe's column into a list of the separate words"""
-    
+
+    """A function that splits the sentences in a dataframe's
+    column into a list of the separate words"""
+
     result = []
     l1 = df['Tweets']
     for tweet in l1:
-         result.append(tweet.lower().split(' '))
+        result.append(tweet.lower().split(' '))
     df['Split Tweets'] = result
     return df
+
+### END FUNCTION
