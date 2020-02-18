4e829c1e242effe9189dbf1f3584c79baf3b6842
REFENTSE-MOTLGELWA <60340826+REFENTSE-MOTLGELWA@users.noreply.github.com>
2020-02-18 16:01:49 +0200

    Cleaned function 1
    Cleaned PEP8

--- function_one_ft.py
+++ function_one_ft.py
@@ -1,43 +1,37 @@
-"""A function that calculates the mean, median, variance, standard deviation, 
-minimum and maximum of list of items"""
-
-          
 ###FUNCTION START
-def dictionary_of_metrics(items) :
-    
-    s = 0
+def dictionary_of_metrics(items):
+
+    """A function that calculates the mean, median, variance, standard deviation,
+    minimum and maximum of list of items"""
+    total = 0
     count = 0
-
-    for value in items :
-        s = s + value
+    for value in items:
+        total = total + value
         count = count + 1
-        the_mean = round ( s / count , 2) 
-    
+        the_mean = round(total / count, 2)
     sorted_items = sorted(items)
-
     if count % 2 == 1:
         the_median = sorted_items[int(round(count+1)/2-1)]
     else:
-        lower = sorted_items[int(round(count/2-1))]
-        upper = sorted_items[int(round(count/2))]
-        the_median = (lower + upper) / 2
-    
-    sum_of_sqz = 0
-    for j in items :
+        lower_median = sorted_items[int(round(count/2-1))]
+        upper_median = sorted_items[int(round(count/2))]
+        the_median = (lower_median + upper_median) / 2
+    sum_of_sqz = 0  # Calculate Sum of squares for Varience
+    for j in items:
         sqrz_calc = (j - the_mean)**2
         sum_of_sqz = sum_of_sqz + sqrz_calc
-
-    the_varience = round(sum_of_sqz / (count - 1), 2)  
-
-    the_standard_dev = round((the_varience)**(1/2), 2)  
-
-    the_min = sorted_items[0] 
-
+    the_varience = round(sum_of_sqz / (count - 1), 2)
+    the_standard_dev = round((the_varience)**(1/2), 2)
+    the_min = sorted_items[0]
     the_max = sorted_items[count - 1]
-
-    dict = {'mean':the_mean,'median': the_median,'var':the_varience,
-    'std':the_standard_dev,'min':the_min,'max':the_max}
-
+    dict = {
+        'mean': the_mean,
+        'median': the_median,
+        'var': the_varience,
+        'std': the_standard_dev,
+        'min': the_min,
+        'max': the_max
+        }
     return dict
 
 ### FUNTION END
