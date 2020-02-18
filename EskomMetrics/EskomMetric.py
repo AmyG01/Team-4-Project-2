d24bc3b3e97436fcfda8c2f0253604c5a34d64ec
Philani Mkhize <philaninkanyiso27@gmail.com>
2020-02-18 17:13:40 +0200

    added function four

--- EskomMetrics/EskomMetric.py
+++ EskomMetrics/EskomMetric.py
@@ -65,3 +65,29 @@ def date_parser(dates):
     return final_date
     
 ### END FUNCTION
+
+def extract_municipality_hashtags(df):
+    mun_list = []
+    for tweet in df['Tweets']:
+        mun_list_2 = []
+        [mun_list_2.append(mun_dict[i])for i in mun_dict if i in tweet.split()]
+        mun_list.append(mun_list_2)
+
+    df['municipality'] = mun_list
+    df['municipality'] = df['municipality'].apply(
+        lambda a: a
+        if len(mun_list) != 0 else np.nan)
+
+    hashtags = []
+    for tweet in df['Tweets']:
+        hashtags_2 = []
+        [hashtags_2.append(j.lower()) for j in tweet.split() if j[0] == '#']
+        hashtags.append(hashtags_2)
+    df['hashtags'] = hashtags
+    df['hashtags'] = df['hashtags'].apply(
+        lambda x: x
+        if len(hashtags) != 0 else np.nan)
+    return df
+
+
+### END FUNCTION
