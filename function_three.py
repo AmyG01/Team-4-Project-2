681f7ebec0099ffcebd49bc5c091ba357d559c5a
REFENTSE-MOTLGELWA <60340826+REFENTSE-MOTLGELWA@users.noreply.github.com>
2020-02-18 16:08:23 +0200

    Cleaned function 3
    Cleaned PEP8

--- function_three.py
+++ function_three.py
@@ -1,14 +1,13 @@
-"""A function that takes as input a list of these datetime strings
- and returns only the date in 'yyyy-mm-dd' format"""
-
 ### START FUNCTION
 def date_parser(dates):
 
+    """A function that takes as input a list of these datetime strings
+    and returns only the date in 'yyyy-mm-dd' format"""
+
     final_date = []
-    for date in dates: 
-        final_date = final_date + [date[0:10]]   
-    
-    
+    for date in dates:
+        final_date = final_date + [date[0:10]]
+
     return final_date
 
 ### END FUNCTION
