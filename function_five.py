87927047a211c4f71b7f099182530b4d98038f4a
AmyG01 <60346057+AmyG01@users.noreply.github.com>
2020-02-18 16:08:37 +0200

    Cleaned_function_5

--- function_five.py
+++ function_five.py
@@ -1,19 +1,15 @@
 ### START FUNCTION
-
-"""A function which calculates the number of tweets that were posted per day"""
-
 def number_of_tweets_per_day(df):
-    
+
+    """A function which calculates the number
+    of tweets that were posted per day"""
+
     final_date = []
-    for date in df['Date']: 
+    for date in df['Date']:
         final_date = final_date + [date[0:10]]
-      
     df = df.drop('Date', 1)
-    
     df['Date'] = final_date
-    
-    no_of_tweets = df[['Date','Tweets']].groupby('Date').count()  
-    
+    no_of_tweets = df[['Date', 'Tweets']].groupby('Date').count()
     return no_of_tweets
 
 ### END FUNCTION
