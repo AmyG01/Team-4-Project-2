e55602abe526dcf0062dc71151cb639a7b005cb3
SiboneloJunior <60383036+SiboneloJunior@users.noreply.github.com>
2020-02-18 17:07:37 +0200

    Added Function 3 to Eskom Metrics

--- EskomMetrics/EskomMetric.py
+++ EskomMetrics/EskomMetric.py
@@ -54,3 +54,14 @@ def five_num_summary(items):
     return dict
 
 ### END FUNCTION
+
+### START OF FUNCTION THREE
+def date_parser(dates):
+    """A function that takes as input a list of these datetime strings
+    and returns only the date in 'yyyy-mm-dd' format"""
+    final_date = []
+    for date in dates:
+        final_date = final_date + [date[0:10]]
+    return final_date
+    
+### END FUNCTION
