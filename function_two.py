08b1a1ff03dab2c37fce24fe6d05b50fdda4822d
AmyG01 <60346057+AmyG01@users.noreply.github.com>
2020-02-18 16:05:20 +0200

    Cleaned_function_2

--- function_two.py
+++ function_two.py
@@ -1,19 +1,18 @@
-"""A function which takes in a list of integers and returns a dictionary of the
-the five number summary"""
-
 ### START FUNCTION
 def five_num_summary(items):
+    """A function which takes in a list of integers and returns a dictionary of the
+    the five number summary"""
 
     sort_items = sorted(items)
     minimum = sort_items[0]
     maximum = sort_items[-1]
+    # Calculating & Unpacking percentiles using Numpy
+    x, y, z = np.percentile(items, [25, 50, 75])
+    q1 = round(x, 2)
+    med = round(y, 2)
+    q3 = round(z, 2)
 
-    x,y,z = np.percentile(items, [25, 50, 75])
-    q1 = round(x,2)
-    med = round(y,2)
-    q3 = round(z,2)
-
-    dict = {'max' : maximum, 'median' : med,'min' : minimum,  'q1' : q1 , 'q3' : q3}
+    dict = {'max': maximum, 'median': med, 'min': minimum, 'q1': q1, 'q3': q3}
 
     return dict
 
