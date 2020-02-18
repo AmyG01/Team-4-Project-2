4397aa824e95b3a304b4a5d2095e16830f77fea8
SiboneloJunior <60383036+SiboneloJunior@users.noreply.github.com>
2020-02-18 16:03:24 +0200

    Clean / PEP8

--- function_seven.py
+++ function_seven.py
@@ -1,17 +1,18 @@
+### START FUNCTION
 def stop_words_remover(df):
 
     """A function which removes english stop words from a tweet"""
-
-    
     stop_words = stop_words_dict['stopwords']
-    
-    df['Without Stop Words'] = [' '.join([w for w in x.lower().split() if w not in stop_words]) 
-    for x in df['Tweets'].tolist()]
-    
+
+    df['Without Stop Words'] = [' '.join([w for w in x.lower().split()
+                                if w not in stop_words])
+                                for x in df['Tweets'].tolist()
+                                ]
     result = []
     l1 = df['Without Stop Words']
     for tweet in l1:
         result.append(tweet.split(' '))
     df['Without Stop Words'] = result
-    
     return df
+
+### END FUNCTION
