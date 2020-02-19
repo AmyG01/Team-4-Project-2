d8eba7ee840d76a4b6882c583f8286f8056d4b49
Sibonelo.Malakiya <sbonelojunior@gmail.com>
2020-02-19 02:27:51 +0200

    Updated Docstrings to align with EskomMetrics package

--- function-six.py
+++ function-six.py
@@ -1,9 +1,20 @@
 ### START FUNCTION
 def word_splitter(df):
+    """
+    Splits the sentences of a specific dataframe column into a list of the
+    separate words
 
-    """A function that splits the sentences in a dataframe's
-    column into a list of the separate words"""
+    Arg :
+        df : Pandas Dataframe
 
+    Returns :
+        df : Modified original dataframe where the specific column sentences
+        are now a list of seperated lowercase words
+
+    Example :
+        >>>df.column_name = @ArthurGodbeer Is the power restored as yet?
+        [@arthurgodbeer, is, the, power, restored, as, yet, ?]
+    """
     result = []
     l1 = df['Tweets']
     for tweet in l1:
