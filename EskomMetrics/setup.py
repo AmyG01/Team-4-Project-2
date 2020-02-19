d9211f47b49dde852854a4c736d6d66a285de0b4
Philani Mkhize <philaninkanyiso27@gmail.com>
2020-02-19 13:22:27 +0200

    edited

--- EskomMetrics/setup.py
+++ EskomMetrics/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='EskomMetrics',
-    version='0.2',
+    version='0.1',
     packages=find_packages(exclude=['tests*']),
     license='MIT',
     description='EDSA example python package',
