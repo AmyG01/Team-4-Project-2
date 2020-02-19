74780b640c54713b9d73773304025850e0381a4f
SiboneloJunior <60383036+SiboneloJunior@users.noreply.github.com>
2020-02-19 16:46:51 +0200

    Final Updates with function 4

--- EskomMetrics/EskomMetric.py
+++ EskomMetrics/EskomMetric.py
@@ -121,49 +121,42 @@ def date_parser(dates):
 ### START OF FUNCTION FOUR
 def extract_municipality_hashtags(df):
     """
-    a function which takes in a pandas dataframe and returns a modified
+    A function which takes in a pandas dataframe and returns a modified
     dataframe that includes two new columns that contain information about
     the municipality and hashtag of the tweet
-
     Arg:
         df : takes a pandas dataframe as input
-
     Result:
-        mun_list_2 : returns a column of information about Municipality 
+        mun_list_2 : returns a column of information about Municipality
         hashtags   : returns a column of hashtag of the tweet
-    
     Example:
         >>>extract_municipality_hashtags(twitter_df.copy())
         "return the following as a dataframe"
-        [0| @BongaDlulane Please send email to mediades...| 2019-11-29 12:50:54| NaN| NaN]
-        [1| @saucy_mamiie Pls log a call on 0860037566 2019-11-29| 12:46:53| NaN| NaN]
+        [0| @BongaDlulane Please send email to mediades...|
+        2019-11-29 12:50:54| NaN| NaN]
+        [1| @saucy_mamiie Pls log a call on 0860037566|
+        2019-11-29| 12:46:53| NaN| NaN]
         ...
-        [195| Eskom's Visitors Centresâ€™ facilities include i...| 2019-11-20| 10:29:07| NaN| NaN]
-        [196| #Eskom connected 400 houses and in the process...| 2019-11-20 10:25:20| NaN| [#eskom,
+        [195| Eskom's Visitors Centresâ€™ facilities include i...|
+        2019-11-20| 10:29:07| NaN| NaN]
+        [196| #Eskom connected 400 houses and in the process...|
+        2019-11-20 10:25:20| NaN| [#eskom,
          #eskom, #poweringyourworld]]
     """
-    #instantiate an empty list for adding splitted tweet as a list
     mun_list = []
     for tweet in df['Tweets']:
         mun_list_2 = []
-        [mun_list_2.append(mun_dict[i])for i in mun_dict if i in tweet.split()]
+        [mun_list_2.append(mun_dict[i]) for i in mun_dict if i in tweet.split()]
         mun_list.append(mun_list_2)
-    #check validity of municipality information in the list
     df['municipality'] = mun_list
-    df['municipality'] = df['municipality'].apply(
-        lambda a: a
-        if len(mun_list) != 0 else np.nan)
-    #instantiate an empty list for adding hashtags
+    df['municipality'] = df['municipality'].apply(lambda mun_list: mun_list if len(mun_list) != 0 else np.nan)
     hashtags = []
     for tweet in df['Tweets']:
         hashtags_2 = []
         [hashtags_2.append(j.lower()) for j in tweet.split() if j[0] == '#']
         hashtags.append(hashtags_2)
-    #check validity of hashtags in the list
     df['hashtags'] = hashtags
-    df['hashtags'] = df['hashtags'].apply(
-        lambda x: x
-        if len(hashtags) != 0 else np.nan)
+    df['hashtags'] = df['hashtags'].apply(lambda hashtags: hashtags if len(hashtags) != 0 else np.nan)
     return df
 
 ### END FUNCTION
